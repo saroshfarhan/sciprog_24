f45d9abb0ba2b242babd35ff1d9406e45c0cac41
Sarosh Farhan <saroshfarhan6495@gmail.com>
2024-10-22 00:10:54 +0100

    fibonacci completed fpr practical 5

---
+++ practical5/fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void update_fibonacci(int *fn1, int *fn2);
+
+int main(void)
+{
+
+    int n;
+    int f0 = 0, f1 = 1;
+
+    printf("Enter a number till which you want Fibonacci sequence: \n");
+    scanf("%d", &n);
+
+    printf("%d ", f0);
+
+    if (n > 0)
+    {
+        printf("%d ", f1);
+    }
+
+    int i;
+    for (i = 2; i <= n; i++)
+    {
+        update_fibonacci(&f1, &f0);
+        printf("%d ", f1);
+    }
+
+    printf("\n");
+    return 0;
+}
+
+void update_fibonacci(int *fn1, int *fn2)
+{
+    int fn = *fn1 + *fn2;
+    *fn2 = *fn1;
+    *fn1 = fn;
+}
