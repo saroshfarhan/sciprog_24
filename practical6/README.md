7a18c23a9dccb8f007c026f997dba175a88a2a49
Sarosh Farhan <saroshfarhan6495@gmail.com>
2024-10-29 13:17:34 +0000

    updated practical 6, need to update makefil as well.

---
+++ practical6/README.md
@@ -0,0 +1,8 @@
+### Commands to make file
+
+```shell
+saroshfarhan@Saroshs-MacBook-Air step6 % gcc -c main.c
+saroshfarhan@Saroshs-MacBook-Air step6 % gcc -c matMul.c
+saroshfarhan@Saroshs-MacBook-Air step6 % gcc -o matMultC main.o matMul.o
+```
+
---
+++ practical6/step6/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+// function prototype for matmul
+void matmul(int n, int p, int q, double A[n][p], double B[p][q], double C[n][q]);
+
+int main(void)
+{
+    int n = 5, p = 3, q = 4;
+    double A[n][p], B[p][q], C[n][q];
+    int i, j, k; // loop indices
+    // initialize A,B ,C
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < p; j++)
+        {
+            A[i][j] = i + j;
+        }
+    }
+
+    for (i = 0; i < p; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            B[i][j] = i - j;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            C[i][j] = 0.0;
+        }
+    }
+
+    // perform matrix multiplication
+    matmul(n, p, q, A, B, C);
+
+    // print out the matrices
+    printf("\n This is matrix A \n\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < p; j++)
+        {
+            printf("%3.0lf ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n This is matrix B \n\n");
+    for (i = 0; i < p; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            printf("%3.0lf ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n This is matrix C \n\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            printf("%3.0lf ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
